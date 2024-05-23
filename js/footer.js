d462fe44464c121708af62e9c30487ffa69a1817
N Javier Diaz <njavi.diaz@gmail.com>
2024-05-22 21:37:41 -0300

    Update

---
+++ js/footer.js
@@ -0,0 +1,21 @@
+const footer_item = `<footer>
+    <section id="contacto">
+    <h3>Contactanos en todas nuestras redes sociales</h3>
+
+    <div class="container">
+    <ul class="contact-footer">
+     <a href="tel:+542664250349"><img src="media/auricular-phone-symbol-in-a-circle_icon-icons.com_56570.png" alt=""><span>2664250349</span></a>
+     <a href="tel:+542664846784"><img src="media/auricular-phone-symbol-in-a-circle_icon-icons.com_56570.png" alt=""><span>2664846784</span></a>
+     <a href="mailto:maximilianopotrero@gmail.com"> <img src="media/email.png" alt=""></a>
+ <a href="https://www.facebook.com/profile.php?id=100083484181892"> <img src="media/fb_icon-icons.com_65434.svg" alt=""></a>
+ <a href="https://www.instagram.com/homeviviendas"> <img src="media/instagram_icon-icons.com_65435.svg" alt=""></a>
+    </ul>
+     </div>
+
+    <div class="container">
+        <p>&copy; MaxiCasa. Todos los derechos reservados. <br>
+            Los precios y descripciones en detalle se encuentran sujeto a cambios</p>
+    </div>
+    </section>
+
+</footer>`
